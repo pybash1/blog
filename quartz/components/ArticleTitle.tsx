3a871da6c66c429aa2e0bb4ca127ef3079254e7e
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-12-10 17:38:50 +0530

    fix: fixed titles

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,7 +3,7 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 function ArticleTitle({ fileData, displayClass }: QuartzComponentProps) {
   if (fileData.slug === "index") {
     return (
-      <h1 class={`article-title ${displayClass ?? ""}`}>
+      <h1 class={`article-title index-title ${displayClass ?? ""}`}>
         Writing about <i class="keyword">crafting</i> perfection and other{" "}
         <i class="keyword">tidbits</i>
       </h1>
@@ -21,8 +21,11 @@ function ArticleTitle({ fileData, displayClass }: QuartzComponentProps) {
 ArticleTitle.css = `
 .article-title {
   margin-top: 15rem;
-  font-weight: 400;
   font-size: 3rem;
+}
+
+.index-title {
+  font-weight: 400;
 }
 
 .keyword {
