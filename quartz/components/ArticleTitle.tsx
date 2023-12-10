bfaa077739ce03bb644097b1356054a4a0a70070
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-12-10 17:14:19 +0530

    feat: updated how the title looks

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,6 +1,15 @@
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 function ArticleTitle({ fileData, displayClass }: QuartzComponentProps) {
+  if (fileData.slug === "index") {
+    return (
+      <h1 class={`article-title ${displayClass ?? ""}`}>
+        Writing about <i class="keyword">crafting</i> perfection and other{" "}
+        <i class="keyword">tidbits</i>
+      </h1>
+    )
+  }
+
   const title = fileData.frontmatter?.title
   if (title) {
     return <h1 class={`article-title ${displayClass ?? ""}`}>{title}</h1>
@@ -8,9 +17,16 @@ function ArticleTitle({ fileData, displayClass }: QuartzComponentProps) {
     return null
   }
 }
+
 ArticleTitle.css = `
 .article-title {
-  margin: 2rem 0 0 0;
+  margin-top: 15rem;
+  font-weight: 400;
+  font-size: 3rem;
+}
+
+.keyword {
+  font-family: 'Newsreader';
 }
 `
 
